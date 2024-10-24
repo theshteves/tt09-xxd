6e49d939d52b1d712ff0fb768d08226c253b798f
theshteves <stevenkneiser@gmail.com>
2024-10-23 23:06:56 -0400

    Generated fibonacci sequence generator

--- src/project.v
+++ src/project.v
@@ -17,11 +17,22 @@ module tt_um_xxd_theshteves (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 1;
+  assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  reg [7:0] next_fib;
+  
+  always @(posedge clk or negedge rst_n) begin
+      if (!rst_n) begin
+          uo_out <= 1;
+          next_fib <= 1;
+      end else begin
+          uo_out <= next_fib;
+          next_fib <= ou_out + next_fib;
+      end
+  end
 
 endmodule
