e47f001333ec2994ae15eed74619e7811d42a21e
theshteves <stevenkneiser@gmail.com>
2024-10-24 00:10:31 -0400

    Matched testbench expectations w/ 8-bit fibonacci sequence

--- test/test.py
+++ test/test.py
@@ -1,10 +1,14 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
 
+import functools
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
+@functools.lru_cache
+def fib(n): return fib(n-1) + fib(n-2) if n > 1 else 1
 
 @cocotb.test()
 async def test_project(dut):
@@ -29,12 +33,10 @@ async def test_project(dut):
     dut.ui_in.value = 20
     dut.uio_in.value = 30
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    #TODO: handle 8-bit overflow
+    for n in range(13):
+        expected_fibonacci = fib(n)
+        #print(f'{n} => {expected_fibonacci}')
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        await ClockCycles(dut.clk, 1)
+        assert dut.uo_out.value == expected_fibonacci
